0182726e93ee9fab606536eb7f202a6e85666ccd
liqing <liqing.qiya@gmail.com>
2014-11-17 23:54:51 +0800

    未定义行为赋值，越界赋值，内存泄漏

--- linux/checker.c
+++ linux/checker.c
@@ -4,13 +4,13 @@
 int main(){
     char *ptr = (char *) malloc(1024);
     char ch;
-    /*Uninitialized read*/
+    /*Uninitialized read, 未定义*/
     ch = ptr[1024];
 
-    /*Write beyond the block*/
+    /*Write beyond the block, 越界*/
     ptr[1024] = 0;
 
-    /*Orphan the block*/
+    /*Orphan the block, 内存泄漏*/
     ptr = 0;
     exit(0);
 }
